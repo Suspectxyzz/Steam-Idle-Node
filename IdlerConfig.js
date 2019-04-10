ee8972432e84f4d8ec83333bc870a4a4e7894249
Alex <45514952+xSuspect@users.noreply.github.com>
2019-04-10 19:58:23 +0300

    Add files via upload

---
+++ IdlerConfig.js
@@ -0,0 +1,24 @@
+ var steamClientFactory = require('./steamClient.js');
+    var configsArray = [];
+    var config;
+    var botArray = [];
+    
+    config = {};
+    config.username = 'USER';
+    config.password = 'PASSWORD';
+    config.sharedSecret = ''; //Shared Secret(for 2FA only) https://github.com/Jessecar96/SteamDesktopAuthenticator/releases
+    config.games = [730]
+    configsArray.push(config);
+	
+	
+    console.log('Number of configurations set up: ' + configsArray.length);
+     
+    for	(index = 0; index < configsArray.length; index++) {
+    	var config = configsArray[index];
+		
+		var bot = steamClientFactory.buildBot(config);
+		bot.doLogin();
+		botArray.push(bot);
+    }
+     
+    console.log('Running ' + botArray.length + ' bots.');
---
+++ StartIdle.cmd
@@ -0,0 +1,4 @@
+cd D:\Steam Idle
+mode con:cols=50 lines=15
+node IdlerConfig.js
+pause
